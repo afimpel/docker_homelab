3c2e62746dde2c897c0f63f971053086ec253768
afimpel <afimpel@afimpel.com>
2024-09-20 20:24:08 -0300

    feat: :fire: Dumps SQL
    dumps

--- DOCKER/images/mariadb/dump_databases.sh
+++ DOCKER/images/mariadb/dump_databases.sh
@@ -6,10 +6,15 @@ mkdir -p $DUMP_DIR
 # Obtener la lista de bases de datos
 DBS=$(mariadb -u root -p$MARIADB_ROOT_PASSWORD -e 'SHOW DATABASES;' | grep -Ev "(Database|information_schema|performance_schema|mysql|sys)")
 date > $DUMP_DIR/dumps.md
+unix=$(date '+%s')
 echo -e "# Dumping database:\n" >> $DUMP_DIR/dumps.md
 # Realizar el dump de cada base de datos
 for db in $DBS; do
     echo "  ⛁ $db"
-    echo " *  ⛁ $db > $DUMP_DIR/$db.sql" >> $DUMP_DIR/dumps.md
+    echo -e " *  ⛁ $db :: $DUMP_DIR/$db.sql" >> $DUMP_DIR/dumps.md
     mariadb-dump -u root -p$MARIADB_ROOT_PASSWORD --databases $db > $DUMP_DIR/$db.sql
 done
+cd $DUMP_DIR
+echo "  "
+echo "  ✔ $unix-SQL_backup.tgz"
+tar -cvzf $unix-SQL_backup.tgz *.sql
