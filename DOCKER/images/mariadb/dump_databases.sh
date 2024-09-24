1338e1287b9447deb12e06fb98463450ef1c62ff
afimpel <afimpel@afimpel.com>
2024-09-23 21:13:44 -0300

    refactor(add): :white_check_mark: add feature
    dumps ALL
    dump Database
    clear
    down clear

--- DOCKER/images/mariadb/dump_databases.sh
+++ DOCKER/images/mariadb/dump_databases.sh
@@ -2,22 +2,32 @@
 # Directorio donde se guardarán los dumps
 DUMP_DIR="/root/dumps"
 mkdir -p $DUMP_DIR
-
+db="$1"
 # Obtener la lista de bases de datos
-DBS=$(mariadb -u root -p$MARIADB_ROOT_PASSWORD -e 'SHOW DATABASES;' | grep -Ev "(Database|information_schema|performance_schema|mysql|sys)")
-date > $DUMP_DIR/dumps.md
-unix=$(date '+%s')
-echo -e "# Dumping database:\n" >> $DUMP_DIR/dumps.md
-# Realizar el dump de cada base de datos
 cd $DUMP_DIR
-for db in $DBS; do
-    mariadb-dump -u root -p$MARIADB_ROOT_PASSWORD --databases $db > $db.sql
-    size=$(stat -c %s $db.sql)
+if [ -n "$db" ]; then
+    date > dumps.md
+    echo -e "# Dumping database:\n" >> dumps.md
+    mariadb-dump -u root -p$MARIADB_ROOT_PASSWORD --databases "$db" --compact > ${db}.sql
+    size=$(stat -c %s ${db}.sql)
     echo "  ⛁ $db ($size bytes)"
-    echo -e " *  ⛁ $db :: $db.sql ($size bytes)" >> dumps.md
-done
-echo "  "
-tar -czf $unix-SQL_backup.tgz *.sql
-size=$(stat -c %s $unix-SQL_backup.tgz)
-echo "  🗃 $unix-SQL_backup.tgz ($size bytes)"
-echo -e "\n 🗃 Compressed :: $unix-SQL_backup.tgz ($size bytes)" >> dumps.md
+    echo -e " *  ⛁ $db :: ${db}.sql ($size bytes)" >> dumps.md
+else
+    DBS=$(mariadb -u root -p$MARIADB_ROOT_PASSWORD -e 'SHOW DATABASES;' | grep -Ev "(Database|information_schema|performance_schema|mysql|sys)")
+    date > dumps.md
+    unix=$(date '+%s')
+    echo -e "# Dumping database:\n" >> dumps.md
+    # Realizar el dump de cada base de datos
+    for db in $DBS; do
+        mariadb-dump -u root -p$MARIADB_ROOT_PASSWORD --databases $db --compact > ${db}-dump.sql
+        size=$(stat -c %s ${db}-dump.sql)
+        echo "  ⛁ $db ($size bytes)"
+        echo -e " *  ⛁ $db :: ${db}-dump.sql ($size bytes)" >> dumps.md
+    done
+    echo "  "
+    tar -czf $unix-SQL_backup.tgz *-dump.sql dumps.md
+    rm *-dump.sql
+    size=$(stat -c %s $unix-SQL_backup.tgz)
+    echo "  🗃 $unix-SQL_backup.tgz ($size bytes)"
+    echo -e "\n 🗃 Compressed :: $unix-SQL_backup.tgz ($size bytes)" >> dumps.md
+fi
--- DOCKER/scripts/help.sh
+++ DOCKER/scripts/help.sh
@@ -13,21 +13,23 @@ help()
    echo -e "\t➤ ${LIGTH_GREEN}./homelab${NC} <cmd> <options>"
    echo -e ""
    R1 $WHITE 'Commands (cmd) : ' $LIGTH_GREEN '✔' " " 
-   echo -e "\t➤ ${LIGTH_CYAN}help${NC}\t\t\t\t\tDisplay usage information (this message)"
+   echo -e "\t➤ ${LIGTH_CYAN}help${NC}\t\t\t\t\tDisplay usage information. (this message)"
    if ! [ -f "${COMPOSE_PROJECT_NAME,,}.md" ]; then
-      echo -e "\t➤ ${LIGTH_CYAN}install${NC}\t\t\t\tInstall this project"
+      echo -e "\t➤ ${LIGTH_CYAN}install${NC}\t\t\t\tInstall this project."
    else
       if [ -f "logs/startup.pid" ]; then
          php7=$(docker_bash "homelab-php7" "php" -v | head -1 | cut -d " " -f 2)
          php8=$(docker_bash "homelab-php8" "php" -v | head -1 | cut -d " " -f 2)
-         echo -e "\t➤ ${LIGTH_CYAN}dumps${NC}\t\t\t\t\tbackup of all databases of all mariadb complete (excluding system db)"
-         echo -e "\t➤ ${LIGTH_CYAN}ps${NC}\t\t\t\t\tPrint started containers"
-         echo -e "\t➤ ${LIGTH_CYAN}restart${NC}\t\t\t\tRestart containers"
-         echo -e "\t➤ ${LIGTH_CYAN}down${NC}\t\t\t\t\tStop & down containers"
-         echo -e "\t➤ ${LIGTH_CYAN}logs <container_name>${NC}\t\t\tDocker logs"
-         echo -e "\t➤ ${LIGTH_CYAN}clear ${NC}\t\t\t\tClear all logs & restart containers"
+         echo -e "\t➤ ${LIGTH_CYAN}dumps${NC}\t\t\t\t\tBackup of all databases of all mariadb complete. (excluding system db)"
+         echo -e "\t➤ ${LIGTH_CYAN}dumps <database_name>${NC}\t\t\tBackup a complete mariadb specified database."
+         echo -e "\t➤ ${LIGTH_CYAN}ps${NC}\t\t\t\t\tPrint started containers."
+         echo -e "\t➤ ${LIGTH_CYAN}restart${NC}\t\t\t\tRestart containers."
+         echo -e "\t➤ ${LIGTH_CYAN}down${NC}\t\t\t\t\tStop & Down containers."
+         echo -e "\t➤ ${LIGTH_CYAN}down clear${NC}\t\t\t\tStop, Down containers & Clear all logs."
+         echo -e "\t➤ ${LIGTH_CYAN}logs <container_name>${NC}\t\t\tDocker logs."
+         echo -e "\t➤ ${LIGTH_CYAN}clear ${NC}\t\t\t\tClear all logs & Restart containers."
          echo -e ""
-         echo -e "\t➤ ${LIGTH_CYAN}bash <container_name>${NC}\t\t\tDocker bash in container"
+         echo -e "\t➤ ${LIGTH_CYAN}bash <container_name>${NC}\t\t\tDocker bash in container."
          echo -e "\t➤ ${LIGTH_CYAN}php7${NC}\t\t\t\t\tDocker bash in ${YELLOW}PHP $php7${NC} ( ${RED}♚ root${NC} )"
          echo -e "\t➤ ${LIGTH_CYAN}php7-cli${NC}\t\t\t\tDocker CLI in ${YELLOW}PHP $php7${NC} ( ${RED}♚ root${NC} )"
          echo -e "\t➤ ${LIGTH_CYAN}php7-usr${NC}\t\t\t\tDocker bash in ${YELLOW}PHP $php7${NC} ( ${CYAN}♟ ${USERNAME}${NC} )"
@@ -35,11 +37,11 @@ help()
          echo -e "\t➤ ${LIGTH_CYAN}php8-cli${NC}\t\t\t\tDocker CLI in ${YELLOW}PHP $php8${NC} ( ${RED}♚ root${NC} )"
          echo -e "\t➤ ${LIGTH_CYAN}php8-usr${NC}\t\t\t\tDocker bash in ${YELLOW}PHP $php8${NC} ( ${CYAN}♟ ${USERNAME}${NC} )"
          echo -e ""
-         echo -e "\t➤ ${LIGTH_CYAN}listsite ${NC}\t\t\t\tList all Sites"
-         echo -e "\t➤ ${LIGTH_CYAN}newsite <site> <type>${NC}\t\t\tCreate the New SubDomain ( ${GREEN}<site>.${COMPOSE_PROJECT_NAME,,}.local${NC} )"
-         echo -e "\t\t\t\t\t\t${GREEN}<type>${NC} ( php7, php8 y build )"
+         echo -e "\t➤ ${LIGTH_CYAN}listsite ${NC}\t\t\t\tList all Sites."
+         echo -e "\t➤ ${LIGTH_CYAN}newsite <site> <type>${NC}\t\t\tCreate the New SubDomain. ( ${GREEN}<site>.${COMPOSE_PROJECT_NAME,,}.local${NC} )"
+         echo -e "\t\t\t\t\t\t${GREEN}<type>${NC} ( ${CYAN}build${NC}, ${CYAN}php7${NC} & ${CYAN}php8${NC} )"
       else
-         echo -e "\t➤ ${LIGTH_CYAN}up${NC}\t\t\t\t\tStart containers"
+         echo -e "\t➤ ${LIGTH_CYAN}up${NC}\t\t\t\t\tStart containers."
       fi
    fi
 }
