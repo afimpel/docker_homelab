e2db23f84a4264f0d3d07fe3f98ac006f49f9ecf
afimpel <afimpel@afimpel.com>
2024-09-26 17:40:58 -0300

    feat: :rocket: Size Human

--- DOCKER/images/mariadb/dump_databases.sh
+++ DOCKER/images/mariadb/dump_databases.sh
@@ -9,9 +9,9 @@ if [ -n "$db" ]; then
     date > dumps.md
     echo -e "# Dumping database:\n" >> dumps.md
     mariadb-dump -u root -p$MARIADB_ROOT_PASSWORD --databases "$db" --compact > ${db}.sql
-    size=$(stat -c %s ${db}.sql)
-    echo "  ⛁ $db ($size bytes)"
-    echo -e " *  ⛁ $db :: ${db}.sql ($size bytes)" >> dumps.md
+    size=$(du -sh ${db}.sql | awk '{print $1}')
+    echo "  ⛁ $db ($size)"
+    echo -e " *  ⛁ $db :: ${db}.sql ($size)" >> dumps.md
 else
     DBS=$(mariadb -u root -p$MARIADB_ROOT_PASSWORD -e 'SHOW DATABASES;' | grep -Ev "(Database|information_schema|performance_schema|mysql|sys)")
     date > dumps.md
@@ -20,14 +20,14 @@ else
     # Realizar el dump de cada base de datos
     for db in $DBS; do
         mariadb-dump -u root -p$MARIADB_ROOT_PASSWORD --databases $db --compact > ${db}-dump.sql
-        size=$(stat -c %s ${db}-dump.sql)
-        echo "  ⛁ $db ($size bytes)"
-        echo -e " *  ⛁ $db :: ${db}-dump.sql ($size bytes)" >> dumps.md
+        size=$(du -sh ${db}-dump.sql | awk '{print $1}')
+        echo "  ⛁ $db ($size)"
+        echo -e " *  ⛁ $db :: ${db}-dump.sql ($size)" >> dumps.md
     done
     echo "  "
     tar -czf $unix-SQL_backup.tgz *-dump.sql dumps.md
     rm *-dump.sql
-    size=$(stat -c %s $unix-SQL_backup.tgz)
-    echo "  🗃 $unix-SQL_backup.tgz ($size bytes)"
-    echo -e "\n 🗃 Compressed :: $unix-SQL_backup.tgz ($size bytes)" >> dumps.md
+    size=$(du -sh $unix-SQL_backup.tgz | awk '{print $1}')
+    echo "  🗃 $unix-SQL_backup.tgz ($size)"
+    echo -e "\n 🗃 Compressed :: $unix-SQL_backup.tgz ($size)" >> dumps.md
 fi
