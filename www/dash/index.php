6ce2a0b55b992bcdd080d7bd77b12d27fb1d4156
afimpel <afimpel@afimpel.com>
2024-10-07 19:40:10 -0300

    fix: :art: favicon & lang

--- www/dash/index.php
+++ www/dash/index.php
@@ -4,10 +4,11 @@ $directory = '../sites/';
 $files = scandir($directory);
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="utf-8">
+    <link rel="icon" href="https://afimpel.github.io/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LEMP STACK -- <?php echo strtoupper(getenv('COMPOSE_PROJECT_NAME')); ?> </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/spacelab/bootstrap.min.css">
