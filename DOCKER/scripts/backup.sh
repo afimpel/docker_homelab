19ef8c369177cbcb50cf32313a198b1578b91184
afimpel <afimpel@afimpel.com>
2024-10-24 02:05:27 -0300

    fix: :bug: domains

--- DOCKER/scripts/backup.sh
+++ DOCKER/scripts/backup.sh
@@ -15,7 +15,7 @@ backup () {
     fi
     rightH1 $GREEN "FILES: backup" $WHITE '✔' "."
     more /etc/hosts | grep "${COMPOSE_PROJECT_NAME,,}" > hostsfile.conf
-    tar --exclude='*/node_modules/*' --exclude='*/vendor/*' --exclude='*/storage/framework/*' --exclude='*/.git/*' --exclude='*.log' --exclude='*.tgz' -cvzf backup/${unix}-${COMPOSE_PROJECT_NAME,,}_backup.tgz www/domains www/subdomains dumpSQL/database config DOCKER/.env ${COMPOSE_PROJECT_NAME,,}*.md hostsfile.conf mkcert.csv
+    tar --exclude='*/node_modules/*' --exclude='*/vendor/*' --exclude='*/storage/framework/*' --exclude='*/.git/logs/*' --exclude='*/.git/objects/*' --exclude='*.log' --exclude='*.tgz' -cvzf backup/${unix}-${COMPOSE_PROJECT_NAME,,}_backup.tgz www/domains www/subdomains dumpSQL/database config DOCKER/.env ${COMPOSE_PROJECT_NAME,,}*.md hostsfile.conf mkcert.csv
     size=$(du -sh backup/${unix}-${COMPOSE_PROJECT_NAME,,}_backup.tgz | awk '{print $1}')
     CUSTOM_RIGHT $WHITE "backup Done:" $LIGHT_GRAY "${unix}-${COMPOSE_PROJECT_NAME,,}_backup.tgz ($size)" $WHITE "✔" "." "✔" 0
 }
