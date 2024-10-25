f53619a66ce08363893b195ddcb54500fa2c2aba
afimpel <afimpel@afimpel.com>
2024-10-24 21:58:42 -0300

    style: :art: backup design

--- DOCKER/scripts/backup.sh
+++ DOCKER/scripts/backup.sh
@@ -5,19 +5,28 @@
 ############################################################
 
 backup () {
+    startExec=$(date +'%s')
     openCD $0
     unix=$(date '+%Y_%m_%d-%s')
-    rightH1 $WHITE "backup:" $LIGHT_GRAY "✔" "."
-    clearLogs
+    CUSTOM_RIGHT $GREEN "Backup:" $WHITE "$OLDPWD/backup" $WHITE "✔" "." "✔" 0
+    find . -name "*.tgz" -mtime -15 -exec rm {} \;
+    #clearLogs
     if [ -f "logs/startup.pid" ]; then
-        rightH1 $GREEN "DUMPS: dumpSQL/database" $WHITE '✔' "."
+        ln
+        CUSTOM_RIGHT $LIGHT_GRAY "DUMPS:" $WHITE "$OLDPWD/dumpSQL/database" $WHITE "✔" " " "✔" 0
         docker_bash "homelab-mariadb" "dump_databases"
     fi
-    rightH1 $GREEN "FILES: backup" $WHITE '✔' "."
-    find . -name "*.tgz" -mtime -15 -exec rm {} \;
+    ln
+    CUSTOM_RIGHT $LIGHT_GRAY "FILES:" $WHITE "$OLDPWD" $WHITE "✔" " " "✔" 0
     more /etc/hosts | grep "${COMPOSE_PROJECT_NAME,,}" > hostsfile.conf
-    tar --exclude='*/node_modules/*' --exclude='*/vendor/*' --exclude='*/storage/framework/*' --exclude='*/.git/logs/*' --exclude='*/.git/objects/*' --exclude='*.log' --exclude='*.tgz' -cvzf backup/${unix}-${COMPOSE_PROJECT_NAME,,}_backup.tgz www/domains www/subdomains dumpSQL/database config DOCKER/.env ${COMPOSE_PROJECT_NAME,,}*.md hostsfile.conf mkcert.csv
+    tar --exclude='*/node_modules/*' --exclude='*/vendor/*' --exclude='*/storage/framework/*' --exclude='*/.git/logs/*' --exclude='*/.git/objects/*' --exclude='*.log' --exclude='*.tgz' -cvzf backup/${unix}-${COMPOSE_PROJECT_NAME,,}_backup.tgz www/domains www/subdomains dumpSQL/database config DOCKER/.env ${COMPOSE_PROJECT_NAME,,}*.md hostsfile.conf mkcert.csv > logs/backups.log
+    lines=$(wc -l logs/backups.log | cut -d " " -f 1)
+    echo "  ✔ Compressed files: $lines."
     size=$(du -sh backup/${unix}-${COMPOSE_PROJECT_NAME,,}_backup.tgz | awk '{print $1}')
-    CUSTOM_RIGHT $WHITE "backup Done:" $LIGHT_GRAY "${unix}-${COMPOSE_PROJECT_NAME,,}_backup.tgz ($size)" $WHITE "✔" "." "✔" 0
+    echo -e "\n ${unix}-${COMPOSE_PROJECT_NAME,,}_backup.tgz ($size)" >> logs/backups.log
+    echo "  🗃 ${unix}-${COMPOSE_PROJECT_NAME,,}_backup.tgz ($size)"
+    ln
+    timeExec=$(diffTime "$startExec")
+    CUSTOM_RIGHT $WHITE "backup Done:" $LIGHT_GRAY "$timeExec" $WHITE "✔" "." "✔" 0
 }
 
