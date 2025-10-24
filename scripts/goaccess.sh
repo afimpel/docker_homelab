0822e03dc2b1e862d0018d6084e6755c773469e0
Alvaro Fimpel <afimpel@gmail.com>
2025-10-23 21:36:33 -0300

    feat: :sparkles: update data ...

--- scripts/goaccess.sh
+++ scripts/goaccess.sh
@@ -10,13 +10,14 @@ goaccess () {
     unix=$(date '+%Y_%m_%d-%s')
     rightH1 $YELLOW 'GoAccess' $WHITE '☐' "."
     more /etc/hosts | grep "${COMPOSE_PROJECT_NAME,,}"  | sort | uniq > hostsfile.conf
+    echo "# goaccess Sites" > logs/goaccess/serverData-lists.log
     more hostsfile.conf | while read -r ip hostname extras; do
         if [ "$ip" == "#" ]; then
             echo;
         elif [ ! -z "$hostname" ]; then
             colorize $LIGHT_GRAY "✔ $WHITE $hostname"
             sites_name="${hostname//./_}";
-            echo "$hostname > $sites_name.out" >> logs/goaccess/list.log
+            echo "$hostname > $sites_name.out" >> logs/goaccess/serverData-lists.log
             # Si hay hostnames adicionales en la misma línea
             curl -silent "https://$hostname" -o "logs/goaccess/$sites_name.out"
             for extra in $extras; do
@@ -24,15 +25,16 @@ goaccess () {
                 if [ "$URLSnginx" != "nginx" ]; then
                     sites_name2="${extra//./_}";
                     curl -silent "https://$extra" -o "logs/goaccess/$sites_name2.out"
-                    echo "$extra > $sites_name2.out" >> logs/goaccess/list.log
+                    echo "$extra > $sites_name2.out" >> logs/goaccess/serverData-lists.log
                 fi
             done
         fi
     done
-    docker stop homelab-goaccess 1>> logs/goaccess/list.log 2>&1
+    echo -e "\n\n# docker" >> logs/goaccess/serverData-lists.log    
+    docker stop homelab-goaccess 1>> logs/goaccess/serverData-lists.log 2>&1
     sleep 1s
-    docker start homelab-goaccess 1>> logs/goaccess/list.log 2>&1
-    docker restart homelab-webserver 1>> logs/goaccess/list.log 2>&1
+    docker start homelab-goaccess 1>> logs/goaccess/serverData-lists.log 2>&1
+    docker restart homelab-webserver 1>> logs/goaccess/serverData-lists.log 2>&1
     timeExec=$(diffTime "$startExec0000")
     CUSTOM_RIGHT $WHITE "GoAccess Done:" $LIGHT_GRAY "$timeExec" $WHITE "✔" "." "✔" 0
 }
