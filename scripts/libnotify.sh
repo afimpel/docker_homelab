8ccbcf06722e535736d897fd63b2b42ed6120b7d
Alvaro Fimpel <afimpel@gmail.com>
2025-11-10 22:27:42 -0300

    fix: :bug: improve startup date handling in send_notify function by adding conditional check for logs/startup.pid

--- scripts/libnotify.sh
+++ scripts/libnotify.sh
@@ -4,8 +4,12 @@ send_notify() {
     text=$1
     icons=$2
     if [ -x "$(command -v notify-send)" ]; then
-        startup=$(cat logs/startup.pid)
-        startupDate=$(diffTime "$startup")
+        if [ -f "logs/startup.pid" ]; then
+            startup=$(cat logs/startup.pid)
+            startupDate=$(diffTime "$startup")
+        else
+            startupDate="Not running"
+        fi
         /usr/bin/notify-send "Compose use: ${COMPOSE_PROJECT_NAME^^}" "${text} | ${startupDate}" -a "HomeLab" -i ${icons} -t 8000 1>/dev/null 2>&1
     fi
 }
