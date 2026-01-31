36faf3244f61a32a4a55ef74a38cb8847386c6d4
Alvaro Fimpel <afimpel@gmail.com>
2026-01-31 19:26:41 -0300

    feat: :rocket: bash

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -125,31 +125,32 @@ runonce_fn () {
   openCD $0
   if [ -f "logs/startup.pid" ]; then
     rightH1 $YELLOW 'Runonce' $WHITE '☐' "."
-    echo -e "---\t\t ✔ \t${COMPOSE_PROJECT_NAME^^}\t | \t $(date) \t ✔ \t\t---\n" > logs/runonce/00_ALL.log 
+    DATETIME="$(date +%Y%m%d)"
+    echo -e "---\t\t ✔ \t${COMPOSE_PROJECT_NAME^^}\t | \t $(date) \t ✔ \t\t---\n" > logs/runonce/${DATETIME}-00_ALL.log
+    rm -v logs/runonce/*-50*.log >> logs/runonce/${DATETIME}-00_ALL.log 2>&1
     for script in config/runonce/*.sh ; do
         if [ -r "$script" ] ; then
                 startExec0001=$(date +'%s')
                 nombre_archivo=$(basename "${script}")
                 nombre_base="${nombre_archivo%.*}"
-                DATETIME="$(date +%Y%m%d)"
-                nuevo_nombre="${nombre_base}-BASH_${DATETIME}.log"
-                echo -e "---\t\t ✔\t RUN: \t$script\t | \t $(date) \t ✔ \t\t---\n" > logs/runonce/$nuevo_nombre
+                nuevo_nombre="${DATETIME}-50${nombre_base}.log"
+                echo -e "\n---\t\t ✔\t RUN: \t$script\t | \t $(date) \t ✔ \t\t---\n" > logs/runonce/$nuevo_nombre
                 bash -c "bash $script > logs/runonce/int_$nuevo_nombre 2>&1"
-                echo -e "----------- $(date '+%Y-%m-%d %H:%M:%S') -----------\n✔\t RUN: \t$script ➤\n" >> logs/runonce/00_ALL.log
-                cat logs/runonce/int_$nuevo_nombre >> logs/runonce/00_ALL.log
+                echo -e "----------- $(date '+%Y-%m-%d %H:%M:%S') -----------\n✔\t RUN: \t$script ➤\n" >> logs/runonce/${DATETIME}-00_ALL.log
+                cat logs/runonce/int_$nuevo_nombre >> logs/runonce/${DATETIME}-00_ALL.log
                 cat logs/runonce/int_$nuevo_nombre >> logs/runonce/$nuevo_nombre
                 timeExec0=$(diffTime "$startExec0001")
                 echo -e "\n➤\t Time:\t\t$timeExec0\n---\t\t ✔\t DONE: \t$script\t | \t $(date) \t ✔ \t\t---" >> logs/runonce/$nuevo_nombre
-                echo " " >> logs/runonce/00_ALL.log
-                rm -v logs/runonce/int_$nuevo_nombre >> logs/runonce/00_ALL.log 2>&1
-                echo -e "➤\t Time: \t$timeExec0\n➤\t Size: \t$(du -h logs/runonce/$nuevo_nombre)\n----------- $(date '+%Y-%m-%d %H:%M:%S') -----------\n" >> logs/runonce/00_ALL.log
+                echo " " >> logs/runonce/${DATETIME}-00_ALL.log
+                rm -v logs/runonce/int_$nuevo_nombre >> logs/runonce/${DATETIME}-00_ALL.log 2>&1
+                echo -e "➤\t Time: \t$timeExec0\n➤\t Size: \t$(du -h logs/runonce/$nuevo_nombre)\n----------- $(date '+%Y-%m-%d %H:%M:%S') -----------\n" >> logs/runonce/${DATETIME}-00_ALL.log
                 CUSTOM_LEFT $NC "bash $script" $BLUE "$timeExec0" $LIGHT_GREEN "➤" " " "✔" "7"
         fi
     done 
     timeExec=$(diffTime "$startExec0000")
     CUSTOM_RIGHT $WHITE "Runonce Done:" $LIGHT_GRAY "$timeExec" $WHITE "✔" "." "✔" 0
-    echo -e "\nTime excution: $timeExec." >> logs/runonce/00_ALL.log
-    echo -e "➤\t Size: \t$(du -h logs/runonce/00_ALL.log)\n----------- $(date '+%Y-%m-%d %H:%M:%S') -----------" >> logs/runonce/00_ALL.log
+    echo -e "\nTime excution: $timeExec." >> logs/runonce/${DATETIME}-00_ALL.log
+    echo -e "➤\t Size: \t$(du -h logs/runonce/${DATETIME}-00_ALL.log)\n----------- $(date '+%Y-%m-%d %H:%M:%S') -----------" >> logs/runonce/${DATETIME}-00_ALL.log
   fi
 }
 
