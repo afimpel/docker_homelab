6f75b71b8b564eca7d28de9c06a2b78ff8355a82
Alvaro Fimpel <afimpel@gmail.com>
2026-02-20 23:08:41 -0300

    feat: :rocket: add run-rename script for domain and subdomain management

--- scripts/refactor/run-rename.sh
+++ scripts/refactor/run-rename.sh
@@ -8,7 +8,6 @@ if [ -d "www/domains" ]; then
         if [ -r "$configFiles" ] ; then
             nombre_archivo=$(basename "${configFiles}")
             echo -e "➤\t $nombre_archivo\t\t\t⏲ $(date '+%Y-%m-%d %H:%M:%S')\n"
-            CUSTOM_LEFT $NC "$nombre_archivo" $BLUE "$DATETIME" $LIGHT_GREEN "➤" " " "⏲" 7
             sed -i 's|/var/log/nginx/\(domains\|subdomains\)|/var/log/nginx/sites|g' $configFiles
             sed -i 's|/var/www/\(domains\|subdomains\)|/var/www|g' $configFiles
             sed -i 's|/var/www/dash|/var/www-dash|g' $configFiles
@@ -16,12 +15,12 @@ if [ -d "www/domains" ]; then
     done
     echo -e "➤\t Move: domains\t\t\t⏲ $(date '+%Y-%m-%d %H:%M:%S')\n"
     cd www
-    mv -rvf domains/* .
+    mv -vf domains/* .
     cd ..
 fi
 if [ -d "www/subdomains" ]; then
     echo -e "➤\t Move: domains\t\t\t⏲ $(date '+%Y-%m-%d %H:%M:%S')\n"
     cd www
-    mv -rvf subdomains/* .
+    mv -vf subdomains/* .
     cd ..
 fi
