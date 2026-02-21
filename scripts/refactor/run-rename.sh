4427ddb40409dc726e2c90c26cbecc7293c52f89
Alvaro Fimpel <afimpel@gmail.com>
2026-02-20 22:54:51 -0300

    feat: :rocket: add run-rename script for domain and subdomain management

--- scripts/refactor/run-rename.sh
+++ scripts/refactor/run-rename.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
+source $(dirname $0)/../utils.sh
+
 openCD $0
 if [ -d "www/domains" ]; then
     for configFiles in config/nginx-sites/*.conf ; do
         DATETIME="$(date +%Y%m%d)"
         if [ -r "$configFiles" ] ; then
             nombre_archivo=$(basename "${configFiles}")
+            echo -e "➤\t $nombre_archivo\t\t\t⏲ $(date '+%Y-%m-%d %H:%M:%S')\n"
             CUSTOM_LEFT $NC "$nombre_archivo" $BLUE "$DATETIME" $LIGHT_GREEN "➤" " " "⏲" 7
             sed -i 's|/var/log/nginx/\(domains\|subdomains\)|/var/log/nginx/sites|g' $configFiles
             sed -i 's|/var/www/\(domains\|subdomains\)|/var/www|g' $configFiles
             sed -i 's|/var/www/dash|/var/www-dash|g' $configFiles
         fi
     done
-    CUSTOM_LEFT $NC "Move: domains" $BLUE "$DATETIME" $LIGHT_GREEN "➤" " " "⏲" 7
+    echo -e "➤\t Move: domains\t\t\t⏲ $(date '+%Y-%m-%d %H:%M:%S')\n"
     cd www
     mv -rvf domains/* .
     cd ..
 fi
 if [ -d "www/subdomains" ]; then
-    CUSTOM_LEFT $NC "Move: subdomains" $BLUE "$DATETIME" $LIGHT_GREEN "➤" " " "⏲" 7
+    echo -e "➤\t Move: domains\t\t\t⏲ $(date '+%Y-%m-%d %H:%M:%S')\n"
     cd www
     mv -rvf subdomains/* .
     cd ..
 fi
 
-CUSTOM_LEFT $NC "Done ... " $BLUE "$DATETIME" $LIGHT_GREEN "➤" " " "⏲" 7
+echo -e "➤\t Done ...\t\t\t\t⏲ $(date '+%Y-%m-%d %H:%M:%S')\n"
